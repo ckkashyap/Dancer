dc59bb7c13be0d96ad35988c95c221257733569d
CK Kashyap <ckkashyap@gmail.com>
2012-06-03 19:22:05 +0530

    Formula corrected

--- Pose.hs
+++ Pose.hs
@@ -20,11 +20,12 @@ data Pose = Pose {
 
 otherEnd :: Point3D -> Coordinates3D -> Double -> Point3D
 otherEnd (x, y, z) (xa, ya, za) d = (x', y', z') where
-         x' = x + (d * (cos zr) * (sin yr))
-         y' = y - (d * (sin zr)) -- WOW ... so that y grows upwards
-         z' = z + (d * (cos zr) * (cos yr))
+         x' = x + (d * (cos zr) * (cos xr)) -- first project the point to the xy plane then on the x axis
+         y' = y + (d * (cos zr) * (sin xr))
+         z' = z - (d * (sin zr)) -- WOW ... so that y grows upwards
          zr = pi *  za / 180.0
          yr = pi *  ya / 180.0
+         xr = pi *  xa / 180.0
          
                 
 --[0.0,1.0,1.0,2.0,3.0,5.0,8.0,13.0,21.0,34.0,55.0,89.0,144.0,233.0,377.0,610.0,987.0,1597.0,2584.0,4181.0]
@@ -38,6 +39,8 @@ upperLegLength = 34
 lowerLegLength = 55
 neckLength     = 13
 
+addCoordinates3D :: Coordinates3D -> Coordinates3D -> Coordinates3D
+addCoordinates3D (x1, y1, z1) (x2, y2, z2) = (x1 + x2, y1 + y2, z1 + z2)
 
 dancer2triangles :: Pose -> [Triangle3D]
 dancer2triangles d = [ neck, backBone, upperLeftArm, upperRightArm, lowerLeftArm, lowerRightArm, upperLeftLeg, lowerLeftLeg, upperRightLeg, lowerRightLeg]  where
@@ -45,12 +48,12 @@ dancer2triangles d = [ neck, backBone, upperLeftArm, upperRightArm, lowerLeftArm
                  backBoneTop = otherEnd (backBonePosition d) (backBoneAngle d) backBoneLength
 
                  neck = (backBoneTop, backBoneTop, neckTop)
-                 neckTop = otherEnd backBoneTop (neckAngle d) neckLength
+                 neckTop = otherEnd backBoneTop (addCoordinates3D (neckAngle d) (backBoneAngle d)) neckLength
 
                  upperLeftArm = (backBoneTop, backBoneTop, upperLeftArmTip)
-                 upperLeftArmTip = otherEnd backBoneTop (upperLeftArmAngle d) upperArmLength
+                 upperLeftArmTip = otherEnd backBoneTop (addCoordinates3D (upperLeftArmAngle d) (backBoneAngle d)) upperArmLength
                  lowerLeftArm = (upperLeftArmTip, upperLeftArmTip, lowerLeftArmTip)
-                 lowerLeftArmTip = otherEnd upperLeftArmTip (lowerLeftArmAngle d) lowerArmLength
+                 lowerLeftArmTip = otherEnd upperLeftArmTip (addCoordinates3D (addCoordinates3D (lowerLeftArmAngle d) (upperLeftArmAngle d)) (backBoneAngle d)) lowerArmLength
 
                  upperRightArm = (backBoneTop, backBoneTop, upperRightArmTip)
                  upperRightArmTip = otherEnd backBoneTop (upperRightArmAngle d) upperArmLength
